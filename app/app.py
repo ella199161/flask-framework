84eee758319fa2472c87677ec01d2c822d5f94e1
ella199161 <ella199161@hotmail.com>
2018-05-01 14:00:51 -0800

    May1

--- app/app.py
+++ app/app.py
@@ -8,6 +8,8 @@ from bokeh.embed import components
 from bokeh.plotting import figure, output_file, show
 from bokeh.resources import INLINE
 from bokeh.util.string import encode_utf8
+from bokeh.models import ColumnDataSource
+from bokeh.models import HoverTool
 from math import pi
 from bokeh.models import DatetimeTickFormatter
 import pickle
@@ -50,7 +52,7 @@ def project():
 	pipeF = pickle.load(open("pipeF.pickle", "rb"))
 	pipeG = pickle.load(open("pipeG.pickle", "rb"))
 	agree = pickle.load(open("agree.pickle", "rb"))
-	Listing = pd.read_csv('Listing.csv')
+	Listing = pd.read_csv('Listing_2d.csv')
 	Listing['issued'] = date.today()
 	List_col = [i.lower() for i in Listing.columns.values]
 	Listing.columns = List_col
@@ -68,59 +70,44 @@ def project():
 
 
 
-
+	grade = request.args.get('grade')
 	company = request.args.get('company')
-
-	if company == "Prosper":
-		clf = pickle.load(open("Prosper_toy3.pickle", "rb"))
-	grade = request.args.get('grade') 
 	days = request.args.get('days') 
+	if company == "Lending Club" or company == "Lending club":
+		Lending_p = pickle.load(open('Lending_club_fit_param.pickle', 'rb'))
+		para_map = {'A':0 , 'B':1, 'C':2, 'D':3, 'E':4, 'F':5, 'G':6}
+		xx = np.arange(3*365)
+		a,b,c = Lending_p[para_map[grade]]
+		yfit = a * np.exp(-b * xx + c)
+		yy = [min(1, i) for i in yfit]
+		y_end = a * np.exp(-b * 3*365 + c) 
+		y_safe = y_end/yy*100
+		x_pos = int(days)
+		y_pos = a * np.exp(-b * x_pos + c)
+		py_pos = y_end/y_pos*100
+		data = {'days': xx,
+		       'Pay_Off_Prob': y_safe,
+		       'yy': yy}
+		source = ColumnDataSource(data=data)
+		hover = HoverTool(tooltips = [
+		    ("days", "@days"),
+		    ("Pay Off Prob", "@Pay_Off_Prob %"),
+		] )
+		p = figure(plot_width=600,x_range=(0, 1095),y_range=(0, 1.05), plot_height=400, tools= [hover], toolbar_location='left',title="Loan Survival Prob: {}%".format(py_pos))
+		p.circle(x_pos, y_pos, size=20, color="navy", alpha=0.5)
+		p.line("days",'yy',line_width = 2,source = source)
+		p.xaxis.axis_label_text_font_size = '16pt'
+		p.yaxis.axis_label_text_font_size = '16pt'
+		p.xaxis.major_label_text_font_size="14pt"
+		p.xaxis.axis_label = 'Days'
+		p.yaxis.axis_label = 'Pay Off Prob%'		
+		script, div = components(p, INLINE)
+		return render_template('project.html',status = {'code': 1, 'msg': 'good'},  plot = {'script':script, 'div':div})
 
-	filename = 'Listing ' + str(date.today())
-	new_loan = request.args.get('new_loan')
-	print(new_loan)
-	if new_loan:
-		return render_template('project.html',status = {'code': 1, 'msg': 'New Listing'}, name=filename, data=Listing.to_html() ) 
+	return render_template('project.html',status = {'code': 3, 'msg': 'Please Enter Valid info'} ) 
 
-
-	"""Lending_club_clf =1 
-	term = request.args.get('term')
-	home = request.args.get('home')
-	fico = request.args.get('fico')
-	amount = request.args.get('amount')
-	total_acc = request.args.get('total_acc')
-	#income = int(request.args.get('income'))
-	balance = request.args.get('balance')
-	Credit_L = request.args.get('Credit_L')
-	interest = request.args.get('int_rate') 
 
 	
-	#features = pd.Series([int(Credit_L), int(term), int(interest)*1.0/100, int(home), int(fico), int(total_acc)]).reshape(1,6)
-
-	if company and fico:
-		features = pd.Series([int(Credit_L), int(term), int(interest)/100*0.1, int(home), int(fico), int(total_acc)])
-
-		ypred = clf.predict(features.reshape(1,6))
-		print(ypred)
-	
-	if fico:
-		if ypred == 1:
-			return render_template('project.html',status = {'code': 1, 'msg': "GOOD" } ) 
-			
-		else:
-			return render_template('project.html',status = {'code': 1, 'msg': "BAD"} ) 
-	"""	
-
-	risk_dict = {"A": 3.983147797773583,"B": 8.3016729850008204,"C": 13.242679912878174, "D": 20.338340799622756, "E": 26.98618268861479, "F": 33.391248563256795, "G" :34.32795211318701}
-
-	if days:
-		#param = pickle.load(open("Lending_club_fit_param.pickle", "rb"))
-		print(grade)
-		safe = 100 -  risk_dict[grade]*(1095-int(days))/1095 *1.0
-
-		return render_template('project.html',status = {'code': 3, 'msg': safe} ) 
-	else:
-		return render_template('project.html',status = {'code': 2, 'msg': 'Oops'} ) 
 
 @app.route('/about')
 def about():
@@ -159,12 +146,14 @@ def listing():
 		to_load = 'pipe' + risk +'.pickle'
 		pipeX = pickle.load(open(to_load, "rb"))		
 		ListingX = Listing[Listing['grade'] == risk]
+		if len(ListingX) == 0:
+			return render_template('listing.html', status = {'code': 2, 'msg': 'No current listing at this risk! Try again later!!'} )
 		y_pred = pipeX.predict(ListingX)
 		showX = Listing_All[Listing_All['grade'] == risk]
 		goodX = showX[[bool(i) for i in y_pred]]
 		badX = showX[[not bool(i) for i in y_pred]]
 		filename = 'Listing ' + str(date.today())
-		return render_template('listing.html',status = {'code': 1, 'msg': 'A Listing'} ,  name=filename, data_all=showX, data_good = goodX.to_html(),data_bad = badX.to_html())
+		return render_template('listing.html',status = {'code': 1, 'msg': 'A Listing'} ,  name=filename, data_all=showX, data_good = goodX,data_bad = badX)
 
 	else:
 
